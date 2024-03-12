ffe3d16548cb4b191abb5790437ee548e6d98866
opixelum <48217573+opixelum@users.noreply.github.com>
2024-03-12 11:09:03 +0100

    Use <TAB> for auto completion in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd("colorscheme github_dark_high_contrast")
-vim.cmd("set nornu")
--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -3,6 +3,7 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       filesystem = {
+        -- Show hidden files
         filtered_items = {
           hide_dotfiles = false,
           hide_gitignored = false,
@@ -11,10 +12,40 @@ return {
       },
     },
   },
-  { "okuuva/auto-save.nvim", opts = {
-    execution_message = {
-      enabled = false,
+  {
+    "okuuva/auto-save.nvim",
+    opts = {
+      -- Hide message when file is auto saved
+      execution_message = {
+        enabled = false,
+      },
     },
-  } },
-  { "projekt0n/github-nvim-theme" },
+  },
+  {
+    "projekt0n/github-nvim-theme",
+    config = function()
+      vim.cmd("colorscheme github_dark_default")
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, _)
+      local cmp = require("cmp")
+      cmp.setup({
+        -- Use <TAB> to confirm completion
+        mapping = {
+          ["<TAB>"] = cmp.mapping.confirm({ select = true }),
+        },
+        -- Disable completion for text
+        sources = cmp.config.sources({
+          {
+            name = "nvim_lsp",
+            entry_filter = function(entry, _)
+              return require("cmp").lsp.CompletionItemKind.Text ~= entry:get_kind()
+            end,
+          },
+        }),
+      })
+    end,
+  },
 }
