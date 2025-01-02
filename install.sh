8efba690413089e90b6760442469a4f533e19ad9
opixelum <48217573+opixelum@users.noreply.github.com>
2025-01-01 22:44:08 -0400

    feat: install yay & add environment variables

--- install.sh
+++ install.sh
@@ -1,29 +1,56 @@
 #!/bin/bash
 
+echo "Installing Yay..."
+sudo pacman -Syu --needed --noconfirm git base-devel
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si
+cd ..
+rm -rf yay-bin
+yay -Y --gendb --noconfirm
+yay -Syu --devel --noconfirm
+yay -Y --devel --save --noconfirm
+yay -Fy --noconfirm
+
+echo "Installing packages..."
 packages=(
     alacritty
     brave-bin
     discord-canary
+    dunst
+    eza
     fzf  # Zoxide auto-completion dependency
+    gdm
     gimp
     hypridle
+    hyprland
     hyprlock
+    hyprpaper
     hyprsunset
     jetbrains-toolbox
+    nautilus
     neofetch
     obsidian
     signal-desktop
-    swaybg
     tree
     unzip
+    uwsm
+    vim
+    vlc
     wget
+    wofi
     youtube-music-bin
     zoxide
+    zsh
+    zsh-completions
 )
 yay -Syu --noconfirm "${packages[@]}"
 
-echo "Enforcing Wayland for all applications..."
-sudo cp -r usr/share/applications /usr/share/
+# echo "Enforcing Wayland for all applications..."
+# sudo cp -r usr/share/applications /usr/share/
+
+echo "Setting up UWSM..."
+cp -r home/user/.config/uwsm ~/.config
 
 echo "Copying wallpapers..."
 mkdir ~/Pictures/
@@ -43,13 +70,24 @@ echo "Setting up Alacritty..."
 cp -r home/user/.config/alacritty ~/.config/
 
 echo "Setting up Zsh..."
+chsh -s /usr/bin/zsh
+
+echo "Setting up Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp home/user/.zshrc ~/
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "Setting up Powerlevel10k..."
 cp home/user/.p10k.zsh ~/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo "Setting up Vim..."
 cp home/user/.vimrc ~/
 cp -r home/user/.vim/ ~/
 
-echo "Done."
+echo "Setting up GDM..."
+sudo cp gnome-shell-theme.gresource /usr/share/gnome-shell
+
+echo "Done. Restarting session..."
+sudo systemctl enable --now gdm.service
